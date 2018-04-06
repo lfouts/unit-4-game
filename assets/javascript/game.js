dad06e10e7dc178515bbb7269eb4f3caca877ba7
lfouts <lorenfouts@yahoo.com>
2018-04-06 12:56:39 -0400

    working code

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  var wins= 0;
+  var losses= 0;
+  var totalScore;
+  var crystals;
+  var targetNumber;
+   function reset() {
+      totalScore = 0;
+      crystals = {
+        blue_crystal: Math.floor((Math.random() * 12) + 1),
+        white_crystal: Math.floor((Math.random() * 12) + 1),
+        violet_crystal: Math.floor((Math.random() * 12) + 1),
+        purple_crystal: Math.floor((Math.random() * 12) + 1),
+      };
+      console.log(crystals);
+      targetNumber = Math.floor((Math.random() * 120) + 19);
+      console.log(targetNumber);
+      $("#totalScore").text(totalScore);
+    }
+
+    reset();
+      $("a").on('click', function() {
+        totalScore = totalScore + crystals[this.id];
+        $("#totalScore").text(totalScore);
+        console.log(this.id);
+        console.log(totalScore);
+        if (totalScore === targetNumber) {
+          alert("You win!");
+          wins++;
+          $("#wins").text(wins);
+          reset();
+        }
+        if (totalScore > targetNumber) {
+          alert("You lose!");
+          losses++;
+          $("#losses").text(losses);
+          reset();
+        }
+
+      });
+
+  });
